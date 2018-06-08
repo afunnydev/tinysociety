452787cc9a959a140c1413b33d0f36572a43a702
Félix <the2deux@gmail.com>
2018-06-08 11:01:21 -0400

    Removing color widget

--- static/admin/custom-widget.js
+++ static/admin/custom-widget.js
@@ -1,3 +1,3 @@
-import CMS from 'netlify-cms'
-import * as NativeColorWidget from 'netlify-cms-widget-native-color'
-CMS.registerWidget('native-color', NativeColorWidget.Control)
+// import CMS from 'netlify-cms'
+// import * as NativeColorWidget from 'netlify-cms-widget-native-color'
+// CMS.registerWidget('native-color', NativeColorWidget.Control)
