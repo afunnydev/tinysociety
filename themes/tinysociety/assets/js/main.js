7ac1b1f29240c95862cee62ac201a20983e08c81
Félix <the2deux@gmail.com>
2018-06-28 11:04:51 -0400

    Added ScrollSpy

--- themes/tinysociety/assets/js/main.js
+++ themes/tinysociety/assets/js/main.js
@@ -43,17 +43,49 @@ jQuery( document ).ready(function($) {
       innerWrapperSelector: '.sidebar__inner'
   });
   // Sidebar link styling
-  var tableLinks = $('#sidebar a');
+  var tableLinks = $('#sidebar li');
   tableLinks.first().addClass('active');
-  tableLinks.click(function(){
-    tableLinks.removeClass('active');
-    $(this).addClass('active');
-  })
+  // tableLinks.click(function(){
+  //   tableLinks.removeClass('active');
+  //   $(this).addClass('active');
+  // });
+  // Custom ScrollSpy
+  // Cache selectors
+  var lastId,
+      topMenu = $("#Content .toc-h2"),
+      // All list items
+      menuItems = topMenu.find("a"),
+      // Anchors corresponding to menu items
+      scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+  // Bind to scroll
+  $(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = $(this).scrollTop()+100;
+     
+     // Get id of current scroll item
+     var cur = scrollItems.map(function(){
+       if ($(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+     
+     if (lastId !== id) {
+         lastId = id;
+         // Set/remove active class
+         menuItems
+           .parent().removeClass("active")
+           .end().filter("[href='#"+id+"']").parent().addClass("active");
+     }                   
+  });
 });
 jQuery( document ).load(function($) {
-  
-}) 
-var userFeed = new Instafeed({
+  var userFeed = new Instafeed({
       get: 'user',
       userId: '7328857212',
       accessToken: '7328857212.1677ed0.7160b9e79e0542f39a0001cb4cb0eda6',
@@ -65,3 +97,4 @@ var userFeed = new Instafeed({
       template: '<div class="column one-fourth insta-div"><a class="insta-link" href="{{link}}" target="_blank"><img src="{{image}}" class="insta-image" /></a></div>'
   });
   userFeed.run();
+}) 
